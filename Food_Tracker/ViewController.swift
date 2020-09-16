9e46fcbbee8e855127b913cbf0a95958fe3421ca
Ilja Patrushev <slowtnr@gmail.com>
2020-09-16 16:18:10 +0300

    Initial Commit

---
+++ Food_Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food_Tracker
+//
+//  Created by Ilja Patrushev on 16.9.2020.
+//  Copyright © 2020 Ilja Patrushev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
